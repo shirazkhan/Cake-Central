1481fa6a223d71f970e9ef42632712158f6d5152
Shiraz Khan <sshirazz@gmail.com>
2020-08-09 13:16:06 +0100

    Rename grid container names

--- src/App.js
+++ src/App.js
@@ -4,32 +4,33 @@ import styled from 'styled-components';
 const MainGrid = styled.div`
   display: grid;
   grid-template-areas:
-    "NavContainer NavContainer NavContainer"
-    "PrimaryContainer PrimaryContainer SecondaryContainer"
-    "FooterContainer FooterContainer FooterContainer";
+    "Header Header Header"
+    "PrimaryContent PrimaryContent SideBar"
+    "Footer Footer Footer";
   text-align: center;
+  grid-template-rows: 60px 1fr 120px;
 `;
 
-const NavContainer = styled.div`
-  grid-area: NavContainer;
+const Header = styled.div`
+  grid-area: Header;
   background: #9fa;
 
 `;
 
-const PrimaryContainer = styled.div`
-  grid-area: PrimaryContainer;
+const PrimaryContent = styled.div`
+  grid-area: PrimaryContent;
   background: #1aa;
 
 `;
 
-const SecondaryContainer = styled.div`
-  grid-area: SecondaryContainer;
+const SideBar = styled.div`
+  grid-area: SideBar;
   background: #4fc;
 
 `;
 
-const FooterContainer = styled.div`
-  grid-area: FooterContainer;
+const Footer = styled.div`
+  grid-area: Footer;
   background: #f3a;
 
 `;
@@ -38,32 +39,32 @@ function App() {
   return (
     <>
       <MainGrid>
-        <NavContainer>
+        <Header>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
           Nesciunt totam modi quia delectus, 
           nihil blanditiis illo voluptatibus officiis officia quas incidunt sapiente cumque doloribus qui, 
           molestias in est et culpa consequuntur ipsa? 
           Sunt iusto voluptatum ipsam repellendus esse expedita corrupti.
-        </NavContainer>
-        <PrimaryContainer>
+        </Header>
+        <PrimaryContent>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Dolorum voluptatibus illo, cumque delectus molestias ipsum, rerum aliquam harum architecto
           rem doloremquid commodi obcaecati temporibus recusandae! 
           Dolorem in officia eius nisi fugiat libero nam perspiciatis iure, 
           molestiae eveniet consectetur expedita?
-        </PrimaryContainer>
-        <SecondaryContainer>
+        </PrimaryContent>
+        <SideBar>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
           Inventore mollitia neque eaque error illum, beatae, porro velit ullam harum saepe provident aliquam?
           Quod rerum deserunt nemo excepturi saepe dignissimos cupiditate qui hic, iste aspernatur explicabo?
           Illum odit debitis distinctio accusamus.
-        </SecondaryContainer>
-        <FooterContainer>
+        </SideBar>
+        <Footer>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
           Illo at, similique delectus blanditiis labore nesciunt est officiis atque, 
           perspiciatis tenetur, quae ad ipsa ipsum veniam quas enim quod rem adipisci error? 
           Consectetur a odio cumque rerum dolores adipisci quos voluptatem.
-        </FooterContainer>
+        </Footer>
       </MainGrid>
     </>
   );
