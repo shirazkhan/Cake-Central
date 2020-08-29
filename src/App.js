81191b23c027775a53008017144a1a17796ac65e
Shiraz Khan <sshirazz@gmail.com>
2020-08-29 23:34:47 +0100

    PageHeading center

--- src/App.js
+++ src/App.js
@@ -7,59 +7,60 @@ import TestPrimaryContent from './TestPrimaryContent';
 
 function App() {
   return (
-    <>
-      <MainGrid>
-        <NavFilter />
-        <Header>
-          <NavBar>
-            <Brand><h2>Shiraz Khan</h2></Brand>
-            <NavButton>
-              <NavUILabel>
-                <Hamburger>
-                  <NavInputUI />
-                  <HamburgerInner1 /> <HamburgerInner2 />
-                  <NavMobHMenu>
-                    <NavMobHOverflow>
-                      <NavMobHSpacer></NavMobHSpacer>
-                      <NavLink href="/">Articles</NavLink>
-                      <NavLink href="/">Tutorials</NavLink>
-                      <NavLink href="/">Portfolio</NavLink>
-                      <NavLink href="/">Contact</NavLink>
-                      <NavLink href="/">Blahfafaaff</NavLink>
-                      <NavLink href="/">Aohfohaif</NavLink>
-                      <NavLink href="/">Portfakjfolio</NavLink>
-                      <NavLink href="/">DDContact</NavLink>
-                      <NavMobHSpacer></NavMobHSpacer>
-                    </NavMobHOverflow>
-                    <NavMobHFade />
-                  </NavMobHMenu>
-                </Hamburger>
-              </NavUILabel>
-            </NavButton>
-            <NavLinks>
-              <NavLink href="/">Articles</NavLink>
-              <NavLink href="/">Tutorials</NavLink>
-              <NavLink href="/">Portfolio</NavLink>
-              <NavLink href="/">Contact</NavLink>
-            </NavLinks>
-            <NightButton>
-              <SwitchUI>
-                <SwitchInputUI />
-                <SwitchSliderUI />
-              </SwitchUI>
-            </NightButton>
-          </NavBar>
-        </Header>
-        
-        <TestPrimaryContent />
-        <Footer>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Illo at, similique delectus blanditiis labore nesciunt est officiis atque, 
-          perspiciatis tenetur, quae ad ipsa ipsum veniam quas enim quod rem adipisci error? 
-          Consectetur a odio cumque rerum dolores adipisci quos voluptatem.
-        </Footer>
-      </MainGrid>
-    </>
+    <MainGrid>
+      <NavFilter />
+      <Header>
+        <NavBar>
+          <Brand><h2>Shiraz Khan</h2></Brand>
+          <NavButton>
+            <NavUILabel>
+              <Hamburger>
+                <NavInputUI />
+                <HamburgerInner1 /> <HamburgerInner2 />
+                <NavMobHMenu>
+                  <NavMobHOverflow>
+                    <NavMobHSpacer></NavMobHSpacer>
+                    <NavLink href="/">Articles</NavLink>
+                    <NavLink href="/">Tutorials</NavLink>
+                    <NavLink href="/">Portfolio</NavLink>
+                    <NavLink href="/">Contact</NavLink>
+                    <NavLink href="/">Blahfafaaff</NavLink>
+                    <NavLink href="/">Aohfohaif</NavLink>
+                    <NavLink href="/">Portfakjfolio</NavLink>
+                    <NavLink href="/">DDContact</NavLink>
+                    <NavMobHSpacer></NavMobHSpacer>
+                  </NavMobHOverflow>
+                  <NavMobHFade />
+                </NavMobHMenu>
+              </Hamburger>
+            </NavUILabel>
+          </NavButton>
+          <NavLinks>
+            <NavLink href="/">Articles</NavLink>
+            <NavLink href="/">Tutorials</NavLink>
+            <NavLink href="/">Portfolio</NavLink>
+            <NavLink href="/">Contact</NavLink>
+          </NavLinks>
+          <NightButton>
+            <SwitchUI>
+              <SwitchInputUI />
+              <SwitchSliderUI />
+            </SwitchUI>
+          </NightButton>
+        </NavBar>
+      </Header>
+
+      {/* Content Goes Here */}
+      <TestPrimaryContent />
+      {/* ///////////////// */}
+
+      <Footer>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Illo at, similique delectus blanditiis labore nesciunt est officiis atque, 
+        perspiciatis tenetur, quae ad ipsa ipsum veniam quas enim quod rem adipisci error? 
+        Consectetur a odio cumque rerum dolores adipisci quos voluptatem.
+      </Footer>
+    </MainGrid>
   );
 }
 
--- src/styled/Content.js
+++ src/styled/Content.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const PageHeading = styled.h1`
-
+    text-align: center;
 `;
