8f8cab6d37baed7933d7c9103cde89811fe3bfaa
Shiraz Khan <sshirazz@gmail.com>
2025-01-20 02:10:04 +0000

    reconfigure sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.cakecentral.co.uk',
-    generateRobotsTxt: true
-  };
+  siteUrl: process.env.SITE_URL || 'https://cakecentral.co.uk', // Replace with your website's URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+  sitemapSize: 7000, // Adjust the number of URLs per sitemap if necessary
+};
